d11854d525f18cb44865d94ee18a9a233b81411b
abgibbs <abgibbs@email.arizona.edu>
2018-04-30 17:46:19 -0700

    Updated figures for report

--- Project/SpinPlotting.py
+++ Project/SpinPlotting.py
@@ -19,32 +19,42 @@ sep_MW_M31 = ( (MW_pos['x'] - M31_pos['x'])**2 + (MW_pos['y'] - M31_pos['y'])**2
 
 plt.figure(1)
 # MW
-plt.subplot(411)
+plt.subplot(311)
 plt.plot(MW_s200['t'], MW_s200['spin'], label='Within $r_{200}$')
 plt.plot(MW_qs200['t'], MW_qs200['spin'], 'r', label='Within $r_{200}/4$')
 plt.ylabel("Spin $\lambda'$")
+plt.axvline(x=4000, color='black', linestyle='--')
+plt.axvline(x=5850, color='black', linestyle='--')
+plt.axvline(x=6200, color='black', linestyle='-')
 plt.legend(loc='upper left')
 plt.title('MW Spin Evolution - Within $r_{200}$')
 # M31
-plt.subplot(412)
+plt.subplot(312)
 plt.plot(M31_s200['t'], M31_s200['spin'], label='Within $r_{200}$')
 plt.plot(M31_qs200['t'], M31_qs200['spin'], 'r', label='Within $r_{200}/4$')
 plt.ylabel("Spin $\lambda'$")
+plt.axvline(x=4000, color='black', linestyle='--')
+plt.axvline(x=5850, color='black', linestyle='--')
+plt.axvline(x=6200, color='black', linestyle='-')
 plt.legend(loc='upper left')
 plt.title('M31 Spin Evolution - Within $r_{200}$')
 # Remnant
-plt.subplot(413)
+plt.subplot(313)
 plt.plot(MWM31_s200['t'][40:], MWM31_s200['spin'][40:], label='Within $r_{200}$') #only plot after galaxies merge
 plt.plot(MWM31_qs200['t'][40:], MWM31_qs200['spin'][40:], 'r', label='Within $r_{200}/4$')
 plt.xlim(0, 12000)
 plt.ylabel("Spin $\lambda'$")
+plt.axvline(x=4000, color='black', linestyle='--')
+plt.axvline(x=5850, color='black', linestyle='--')
+plt.axvline(x=6200, color='black', linestyle='-')
 plt.legend(loc='upper left')
 plt.title('Remnant Spin Evolution - Within $r_{200}$')
+'''
 # MW and M31 separation
 plt.subplot(414)
 plt.plot(MW_pos['t'], sep_MW_M31)
 plt.xlabel('Time (Myr)')
 plt.ylabel('MW-M31 sep (kpc)')
 plt.title('MW-M31 Distance')
-
+'''
 plt.show()
--- Project/velANI_plotting.py
+++ Project/velANI_plotting.py
@@ -16,35 +16,45 @@ sep_MW_M31 = ( (MW_pos['x'] - M31_pos['x'])**2 + (MW_pos['y'] - M31_pos['y'])**2
 # plotting 4 graphs, top is milky way va, M31 va, remnant va, bottom is galaxy sep
 plt.figure(1)
 # MW
-plt.subplot(411)
+plt.subplot(311)
 plt.plot(MW_velANI['t'], MW_velANI['r200_velANI'], label='Within $r_{200}$')
 plt.plot(MW_velANI['t'], MW_velANI['qr200_velANI'], 'r', label='Within $r_{200}/4$')
 plt.ylabel('$\\beta$')
 plt.axhline(y=0, color='black')
+plt.axvline(x=4000, color='black', linestyle='--')
+plt.axvline(x=5850, color='black', linestyle='--')
+plt.axvline(x=6200, color='black', linestyle='-')
 plt.legend(loc='upper right')
-plt.title('Milky Way Velocity Aniostropy Evolution')
+plt.title('Milky Way Velocity Anisotropy Evolution')
 # M31
-plt.subplot(412)
+plt.subplot(312)
 plt.plot(M31_velANI['t'], M31_velANI['r200_velANI'], label='Within $r_{200}$')
 plt.plot(M31_velANI['t'], M31_velANI['qr200_velANI'], 'r', label='Within $r_{200}/4$')
 plt.ylabel('$\\beta$')
 plt.axhline(y=0, color='black')
+plt.axvline(x=4000, color='black', linestyle='--')
+plt.axvline(x=5850, color='black', linestyle='--')
+plt.axvline(x=6200, color='black', linestyle='-')
 plt.legend(loc='upper right')
 plt.title('M31 Velocity Anisotropy Evolution')
 # Remnant
-plt.subplot(413)
+plt.subplot(313)
 plt.plot(MWM31_velANI['t'][40:], MWM31_velANI['r200_velANI'][40:], label='Within $r_{200}$') # only plot after merger
 plt.plot(MWM31_velANI['t'][40:], MWM31_velANI['qr200_velANI'][40:], 'r', label='Within $r_{200}/4$')
 plt.ylabel('$\\beta$')
 plt.axhline(y=0, color='black')
+plt.axvline(x=4000, color='black', linestyle='--')
+plt.axvline(x=5850, color='black', linestyle='--')
+plt.axvline(x=6200, color='black', linestyle='-')
 plt.legend(loc='upper right')
 plt.xlim(0, 12000)
 plt.title('Remnant Velocity Anisotropy Evolution')
+'''
 # MW and M31 separation
 plt.subplot(414)
 plt.plot(MW_pos['t'], sep_MW_M31)
 plt.xlabel('Time (Myr)')
 plt.ylabel('MW-M31 sep (kpc)')
 plt.title('MW-M31 Distance')
-
+'''
 plt.show()
